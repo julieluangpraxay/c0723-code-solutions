fb4511343ae44c6f3f9bbc22fd228c5d8c454100
julieluangpraxay <julieluangpraxay@gmail.com>
2023-07-11 19:30:06 +0000

    Completed javascript-data-model exercise - Julie Luangpraxay

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,53 @@
+const orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    delivered: 'Aug 8, 2020',
+    message:
+      'Your package was delivered. It was handed directly to a resident.',
+    itemTitle: 'Javascript for impatient programmers',
+    seller: 'Rauschmayer, Dr. Axel',
+    returnWindow: 'closed on Sep 7, 2020',
+    price: '$31.55',
+  },
+
+  {
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    delivered: 'Jul 20, 2020',
+    message:
+      'Your package was delivered. It was handed directly to a resident.',
+    itemTitle: 'The Timeless Way of Building',
+    seller: 'Alexander, Christopher',
+    returnWindow: 'closed on Aug 19, 2020',
+    price: '$41.33',
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-28883177-2648248',
+    delivered: 'Jul 5, 2020',
+    message:
+      'Your package was delivered. It was handed directly to a resident.',
+    items: [
+      {
+        itemTitle:
+          'Gamecute Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        seller: 'n/a',
+        returnWindow: 'closed on Aug 4, 2020',
+        price: '$15.98',
+      },
+      {
+        itemTitle: 'The Art of Sql',
+        seller: 'Faroult, Stephane',
+        returnWindow: 'closed on Aug 4, 2020',
+        price: '$33.99',
+      },
+    ],
+  },
+];
