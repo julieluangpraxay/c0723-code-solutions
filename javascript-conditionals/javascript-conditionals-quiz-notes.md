7bfa3e535071f4b889c6ea82ee266e7a554ba2a1
julieluangpraxay <julieluangpraxay@gmail.com>
2023-07-13 03:39:21 +0000

    Completed javascript-conditionals exercise -Julie Luangpraxay

---
+++ javascript-conditionals/javascript-conditionals-quiz-notes.md
@@ -0,0 +1,71 @@
+# javascript-conditionals-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- Give 6 examples of comparison operators.
+  Equal to (== or ===): Checks if two values are equal.
+  Example: 5 == 5 (true)
+
+Not equal to (!= or !==): Checks if two values are not equal.
+Example: 5 != 10 (true)
+
+Greater than (>): Checks if the left value is greater than the right value.
+Example: 10 > 5 (true)
+
+Less than (<): Checks if the left value is less than the right value.
+Example: 5 < 10 (true)
+
+Greater than or equal to (>=): Checks if the left value is greater than or equal to the right value.
+Example: 10 >= 10 (true)
+
+Less than or equal to (<=): Checks if the left value is less than or equal to the right value.
+Example: 5 <= 10 (true)
+
+- What data type do comparison expressions evaluate to?
+  boolean
+- What is the purpose of an `if` statement?
+  The purpose of an if statement is to conditionally execute a block of code based on a specific condition or set of conditions
+- Is `else` required in order to use an `if` statement?
+  it is optional but not required
+- Describe the syntax (structure) of an `if` statement.
+  if (condition) {
+  // Code to be executed if the condition is true
+  }
+- What are the three logical operators?
+  or, and, or not.
+- How do you compare two different expressions in the same condition?
+  with logical and operator &&
+- What is the purpose of a `switch` statement?
+  matches expression of clause cases
+- Is a `default` clause required in order to use a `switch` statement?
+  it is optional
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ javascript-conditionals/main.js
@@ -0,0 +1,115 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('isUnderFive value:', isUnderFive(4));
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log('isEven value:', isEven(4));
+
+function startsWithJ(string) {
+  if (
+    string.startsWith('J') &&
+    string.charAt(0) === string.charAt(0).toUpperCase()
+  ) {
+    return true;
+  }
+}
+console.log('startsWithJ value:', startsWithJ('JavaScript'));
+
+function isOldEnoughToDrink(person) {
+  if (person.age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
+}
+const julie = {
+  name: 'Julie',
+  age: 27,
+};
+
+console.log('isOldEnoughToDrink value:', isOldEnoughToDrink(julie));
+
+function isOldEnoughToDrive(person) {
+  if (person.age >= 16) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const joolie = {
+  name: 'joolie',
+  age: 27,
+};
+
+console.log('isOldEnoughToDrive value:', isOldEnoughToDrive(joolie));
+
+function isOldEnoughToDrinkAndDrive(person) {
+  return false;
+}
+console.log('isOldEnoughToDrinkandDrive value:', isOldEnoughToDrinkAndDrive());
+
+function categorizeAcidity(pH) {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH > 0 && pH <= 7) {
+    return 'acidic';
+  } else if (pH >= 7 && pH <= 14) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+}
+
+console.log('categorizeAcidity value:', categorizeAcidity(3));
+
+function introduceWarnerBro(name) {
+  switch (name) {
+    case 'yakko':
+    case 'wakko':
+      return "We're the warner brothers!";
+    case 'dot':
+      return "I'm cute~";
+  }
+}
+console.log(introduceWarnerBro('dot'));
+
+function recommendMovie(genre) {
+  switch (genre) {
+    case 'action':
+      return 'The Avengers';
+    case 'comedy':
+      return 'Step Brothers';
+    case 'horror':
+      return 'Scream';
+    case 'drama':
+      return 'Drama movie';
+    case 'musical':
+      return 'High School Musical';
+    case 'sci-fi':
+      return 'Interstellar';
+  }
+}
+console.log(recommendMovie('action'));
