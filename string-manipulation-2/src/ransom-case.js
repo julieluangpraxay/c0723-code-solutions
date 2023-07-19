1c467eac17ca6e07c18738b6660dc79732b097ef
julieluangpraxay <julieluangpraxay@gmail.com>
2023-07-19 03:37:28 +0000

    WIP

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,29 +1,12 @@
 /* exported ransomCase */
-// function ransomCase(string) {
-// let result = ' ';
-// for (let i = 0; i < string.length; i++) {
-//   const randomNum = Math.random();
-// }
-
-// }
-
 function ransomCase(string) {
-  // Create an empty string to store the result
   let result = '';
-
-  // Loop through each character in the input string
   for (let i = 0; i < string.length; i++) {
-    // Generate a random number between 0 and 1
-    const randomNum = Math.random();
-
-    // Decide whether to convert the character to uppercase or lowercase
-    // If the random number is less than 0.5, convert to uppercase, otherwise, convert to lowercase
-    const modifiedChar =
-      randomNum < 0.5 ? string[i].toUpperCase() : string[i].toLowerCase();
-
-    // Append the modified character to the result string
-    result += modifiedChar;
+    if (i % 2 === 0) {
+      result += string[i].toLowerCase();
+    } else {
+      result += string[i].toUpperCase();
+    }
   }
-
   return result;
 }
