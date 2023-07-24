27b92373b84a9672a7c4c36e283f9e1b21439db3
julieluangpraxay <julieluangpraxay@gmail.com>
2023-07-24 18:48:59 +0000

    Revised code

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,13 +1,16 @@
 const heading = document.querySelector('h1');
 console.log('$heading', heading);
 
-console.dir('$heading', heading);
+console.dir('h1', heading);
 
-const explanation = document.querySelector('explanation');
-console.dir('$explanation', explanation);
+const explanation = document.querySelector('#explanation');
+
+console.log('$explanation', explanation);
+console.dir('p#explanation', explanation);
 
 const hint = document.querySelector('p.hint');
-console.log('$p.hint', hint);
+console.log('$hint', hint);
+
 console.dir('p.hint', hint);
 
 const allParagraphs = document.querySelectorAll('p');
