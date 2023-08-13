fa237d105e06e926cf2eee3b57d1acd3287b90ba
julieluangpraxay <julieluangpraxay@gmail.com>
2023-08-13 00:43:59 +0000

    Completed string-manipulation-2

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,17 @@
 /* exported numVowels */
+function numVowels(string) {
+  let count = 0;
+  string = string.toLowerCase();
+  for (let i = 0; i < string.length; i++) {
+    if (
+      string.charAt(i) === 'a' ||
+      string.charAt(i) === 'e' ||
+      string.charAt(i) === 'i' ||
+      string.charAt(i) === 'o' ||
+      string.charAt(i) === 'u'
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,14 @@
 /* exported swapChars */
+function swapChars(firstIndex, secondIndex, string) {
+  // assign a new array and use split method to turn the string into an array
+  const newArray = string.split('');
+  // assign newArray[firstIndex] to charOne aka temp variable aka placeholder
+  const charOne = newArray[firstIndex];
+  //  reassgin firstindex to secondIndex
+  newArray[firstIndex] = newArray[secondIndex];
+  // use temp variable to set to secondIndex
+  newArray[secondIndex] = charOne;
+  // array method that is concatenating all of the elements in this array
+  // using '' with nothing in between to join them next to each other
+  return newArray.join('');
+}
